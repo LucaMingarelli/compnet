63ba292e056e7d3425717395f4955f1b86951586
Mingarelli, Luca <luca.mingarelli@ecb.europa.eu>
2024-06-26 19:21:27 +0200

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,42 @@
-"""  Created on 26/05/2023::
+"""  Created on 26/06/2024::
 ------------- setup.py -------------
-
+ 
 **Authors**: L. Mingarelli
 """
-
+import os
 import setuptools
-from compnet.__about__ import (__about__, __author__, __email__, __version__, __url__)
 
-with open("README.md", 'r') as f:
+
+with open("README.md", "r") as f:
     long_description = f.read()
+
+about = {}
+with open("compnet/__about__.py") as f:
+    exec(f.read(), about)
+
 
 with open("compnet/requirements.txt") as f:
     install_requirements = f.read().splitlines()
 
-
 setuptools.setup(
     name="compnet",
-    version=__version__,
-    author=__author__,
-    author_email=__email__,
-    description=__about__,
-    url=__url__,
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__email__'],
+    description=about['__about__'],
+    url=about['__url__'],
     license='MIT',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=['compnet', 'compnet.tests'],
-    package_data={'':  ['../compnet/res/*']},
+    long_description_content_type="markdown",
+    packages=setuptools.find_packages() + ['compnet.res'],
+    include_package_data=True,
+    package_data={'':  []},
     install_requires=install_requirements,
-    classifiers=["Programming Language :: Python :: 3",
-                 "License :: OSI Approved :: MIT License",
-                 "Operating System :: OS Independent"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires='>=3.6',
 )
-
-
-
-
 
